7ab89239a3dc01344e0fc91ca24b9ed5ddc4ff3e
sp4m-08 <roysaunok@gmail.com>
2025-01-16 14:34:34 +0530

    landing page done

--- src/components/LandingPage/BookNotes.jsx
+++ src/components/LandingPage/BookNotes.jsx
@@ -1,39 +1,55 @@
 import React from "react";
 import "./BookNotes.css";
+import { useNavigate } from "react-router-dom";
+
+
 
 const BookNotes = () => {
+
+    let navigateNotes = useNavigate(); 
+        const routeNotes = () =>{ 
+        let path = `/notes`; 
+        navigate(path);
+    }
+
+    let navigateBooks = useNavigate(); 
+        const routeBooks = () =>{ 
+        let path = `/books`; 
+        navigate(path);
+    }
     return (
         <div className="booknotes">
-            <div className="books">
-                <h2 className="section-title">Browse Books</h2>
-                <div className="book-list">
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    
-                    
+           
+            <section id="browse-books" className="browse-section">
+                <h2>Browse Books</h2> 
+                <button className="view-more" onClick={routeBooks}>View more</button>
+                <div className="card-container">
+                  {[1, 2, 3, 4].map((item) => (
+                    <div className="card" key={item} onClick={routeBooks}>
+                      <h3>Lorem ipsum dolor sit</h3>
+                      <p className="User"><img src="/pfp.png" alt="books" width="20" height="20" />User </p>
+                      <img src="/white_space.png" alt="books" width="230" height="190" /> 
+                    </div>
+                  ))}
                 </div>
-                <a href="" className="show-more">Show more →</a>
-            </div>
-            <div className="notes">
-                <h2 className="section-title">Browse Notes</h2>
-                <div className="note-list">
-                    <div className="note-item"></div>
-                    <div className="note-item"></div>
-                    <div className="note-item"></div>
-                    <div className="note-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
-                    <div className="book-item"></div>
+              
+                </section>
+
+                <section id="browse-notes" className="browse-section">
+                <h2>Browse Notes</h2> 
+                <button className="view-more" onSubmit={routeNotes}>View more</button>
+                <div className="card-container">
+                 {[1, 2, 3, 4].map((item) => (
+                    <div className="card" key={item} >
+                     <h3>Lorem ipsum dolor sit</h3>
+                     <p className="User"> <img src="/pfp.png" alt="books" width="20" height="20" />User </p>
+                        <img src="/white_space.png" alt="books" width="230" height="190" />
+                   </div>
+                 ))}
                 </div>
-                <a href="" className="show-more">Show more →</a>
-            </div>
+ 
+            </section>
+
         </div>
     );
 };
